31bec4b961af61a585f084060af3ba7b71d88f83
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-10-31 14:27:49 -0700

    clear input function
    clean input function written to rid the calculator of some input bugs and proper use of the brackets

--- script.js
+++ script.js
@@ -28,9 +28,48 @@ for (let key of keys) {
                 input += "(";
             } else if (
                 input.indexOf("(") != -1 && 
-                input.indexOf(")") == -1) {
-                input -= ")";
-            } // this if statement means if there is no first bracket, or if we do have a starting bracket AND we have an outer bracket and the last index of it
+                input.indexOf(")") == -1 || 
+                input.indexOf("(") != -1 &&
+                input.indexOf(")") != -1 &&
+                input.lastIndexOf("(") > input.lastIndexOf(")")
+             ) {
+                input += ")";
+            } 
+            
+            display_input.innerHTML = input;
+            
+            
+            // this if statement means if there is no first bracket, or if we do have a starting bracket AND we have an outer bracket and the last index of it
+        } else {
+            input += value;
+            display_input.innerHTML = input;
         }
     })
+}
+
+// this isn't the most readable but obviously you need some more intuitive javascript to make a calculator with increased functionality to work. is still full of bugs at this point
+
+function cleanInput(input) {
+    let input_array = input.split("");
+    let input_array_length = input_array.length; 
+
+    for(let i = o; i <input_array_length; i++) {
+        if (input_array[i] == "*") {
+            input_array[i] = ` <span class="operator">x</span `;
+        } else if (input_array[i] == "/") {
+            input_array[i] = ` <span class="operator">/</span `;
+        } else if (input_array[i] == "+") {
+            input_array[i] = ` <span class="operator">+</span `;
+        } else if (input_array[i] == "-") {
+            input_array[i] = ` <span class="operator">-</span `;
+        } else if (input_array[i] == "(") {
+            input_array[i] = `<span class="brackets">(</span`; // you do not want spaces on your brackets
+        } else if (input_array[i] == ")") {
+            input_array[i] = `<span class="brackets">)</span`; // you do not want spaces on your brackets 
+        } else if (input_array[i] == "%") {
+            input_array[i] = `<span class="operator">%</span`;
+        }
+
+    }
+    return input_array.join("");
 }
