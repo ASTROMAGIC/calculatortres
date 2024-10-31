3ef92e1cb0fc5e3e62e213ef52ec801c1f74cac9
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-10-31 15:19:56 -0700

    compare contrast
    did a compare contrast to see why adapted js file was not working. input validation would not work unless i returned true, and one of my for loops in detecting numbers had i = o instead of i = 0. many micro errors

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (let key of keys) {
         if(value == "clear") {
             input = "";
             display_input.innerHTML = "";
-            display_output.innerHTML ="";
+            display_output.innerHTML = "";
         } else if(value == "backspace") {
             input = input.slice(0, -1);
             display_input.innerHTML = cleanInput(input);
@@ -56,7 +56,7 @@ function cleanInput(input) {
     let input_array = input.split("");
     let input_array_length = input_array.length; 
 
-    for(let i = 0; i <input_array_length; i++) {
+    for (let i = 0; i < input_array_length; i++) {
         if (input_array[i] == "*") {
             input_array[i] = ` <span class="operator">x</span `;
         } else if (input_array[i] == "/") {
@@ -74,6 +74,7 @@ function cleanInput(input) {
         }
 
     }
+
     return input_array.join("");
 }
 
@@ -115,6 +116,8 @@ function validateInput (value) {
             return true; 
         }
     }
+
+    return true;
 
 }
 
